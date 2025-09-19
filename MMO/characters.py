4cdb04e5e540925438b33d47f47cdc7c899ac7ca
lir147 <120119304+lir147@users.noreply.github.com>
2025-09-19 15:55:27 +0500

    MMO

--- MMO/characters.py
+++ MMO/characters.py
@@ -1,4 +1,3 @@
-
 class Character:
     def __init__(self, name, health, mana, strength, agility, intelligence):
         self.name = name
@@ -16,3 +15,8 @@ class Warrior(Character):
 class Mage(Character):
     def __init__(self, name):
         super().__init__(name, health=60, mana=100, strength=5, agility=10, intelligence=20)
+
+class Archer(Character):
+    def __init__(self, name):
+        # Лёгкий, быстрый класс с высокой ловкостью
+        super().__init__(name, health=80, mana=50, strength=10, agility=20, intelligence=10)
